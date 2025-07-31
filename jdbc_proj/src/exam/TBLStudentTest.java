f67d92cba7543b375a0f6bea4ad6f7d6f39e1850
kimsohee <koreait.sec2020@gmail.com>
2025-07-31 16:33:04 +0900

    day3 jdbc

--- jdbc_proj/src/exam/TBLStudentTest.java
+++ jdbc_proj/src/exam/TBLStudentTest.java
@@ -8,22 +8,25 @@ import util.OracleConnection;
 
 public class TBLStudentTest {
   public static void main(String[] args) {
-    String stuno = System.console().readLine("학번 >>> ");
-    String name = System.console().readLine("이름 >>> ");
-    String temp = System.console().readLine("나이 >>> ");
-    String address = System.console().readLine("주소 >>> ");
+    String stuno = System.console().readLine("학번 >>> "); // not null
+    String name = System.console().readLine("이름 >>> "); // not null
+    String age = System.console().readLine("나이 >>> "); // null 허용
+    String address = System.console().readLine("주소 >>> "); // null 허용
 
     if (stuno.length() == 0 || name.length() == 0) {
       System.out.println("필수 입력입니다.");
     } else {
+      if (age.length() == 0) {
+        age = null;
+      }
       if (address.length() == 0)
         address = null;
-        
+
       insert(stuno, name, age, address);
     }
   }
 
-  private static void insert(String stuno, String name, int age, String address) {
+  private static void insert(String stuno, String name, String age, String address) {
     Connection conn = OracleConnection.getConnection();
     PreparedStatement pstat = null;
     String sql = "insert into tbl_student(stuno,name,age,address) values (?,?,?,?)";
@@ -33,8 +36,8 @@ public class TBLStudentTest {
 
       pstat.setString(1, stuno);
       pstat.setString(2, name);
-      pstat.setInt(3, age);
-      pstat.setString(4, null);
+      pstat.setString(3, age); // setInt(3,null) 불가.
+      pstat.setString(4, address);
       pstat.execute();
       // conn.commit();
       System.out.println("1개 행이 저장되었습니다.");
