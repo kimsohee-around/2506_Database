a815d8f211b8657692af505cdbb9e5c981ebac23
kimsohee <koreait.sec2020@gmail.com>
2025-07-31 17:01:44 +0900

    day3 jdbc

--- jdbc_proj/src/exam/TBLStudentTest.java
+++ jdbc_proj/src/exam/TBLStudentTest.java
@@ -7,7 +7,12 @@ import java.sql.SQLException;
 import util.OracleConnection;
 
 public class TBLStudentTest {
+
   public static void main(String[] args) {
+    updateAddress("2025005", "서울시 구로구 구로동");
+  }
+
+  public static void addStudent() {
     String stuno = System.console().readLine("학번 >>> "); // not null
     String name = System.console().readLine("이름 >>> "); // not null
     String age = System.console().readLine("나이 >>> "); // null 허용
@@ -26,25 +31,42 @@ public class TBLStudentTest {
     }
   }
 
+  private static void updateAddress(String stuno, String address) {
+
+    String sql = "update tbl_student set address = ? where stuno = ?";
+    try ( // try with resources : 자동 close
+        Connection connection = OracleConnection.getConnection();
+        PreparedStatement pstat = connection.prepareStatement(sql);) {
+      connection.setAutoCommit(false);
+      pstat.setString(1, address);
+      pstat.setString(2, stuno);
+      pstat.executeUpdate();
+    } catch (SQLException e) {
+      System.out.println("SQL 예외 : " + e.getMessage());
+    } // finally 없음. 자동 close 구문
+
+  }
+
   private static void insert(String stuno, String name, String age, String address) {
     Connection conn = OracleConnection.getConnection();
     PreparedStatement pstat = null;
     String sql = "insert into tbl_student(stuno,name,age,address) values (?,?,?,?)";
     try {
       conn.setAutoCommit(false);
+      System.out.println("conn :" + conn.getAutoCommit());
       pstat = conn.prepareStatement(sql);
 
       pstat.setString(1, stuno);
       pstat.setString(2, name);
       pstat.setString(3, age); // setInt(3,null) 불가. 오라클에서는 문자열을 number 타입으로 자동캐스팅
       pstat.setString(4, address);
-      pstat.execute();
-      // conn.commit();
+      pstat.executeUpdate();
+      // conn.rollback();
       System.out.println("1개 행이 저장되었습니다.");
     } catch (SQLException e) {
       System.out.println("insert 예외 : " + e.getMessage());
     } finally {
-      OracleConnection.close(conn);
+      // OracleConnection.close(conn);
       try {
         if (pstat != null)
           pstat.close();
