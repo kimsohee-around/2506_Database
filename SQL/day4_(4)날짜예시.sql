1233a1740091cd8f52ba2d39dc5731f00f125aa2
kimsohee <koreait.sec2020@gmail.com>
2025-08-01 15:34:04 +0900

    day4 날짜

--- SQL/day4_(4)날짜예시.sql
+++ SQL/day4_(4)날짜예시.sql
@@ -1,9 +1,9 @@
 -- 예제 테이블
 CREATE TABLE tbl_member (
 	mno NUMBER(5) PRIMARY KEY ,    		--회원번호
-	name varchar2(50) not null,   		-- 이름
+	name varchar2(50) NOT NULL,   		-- 이름
 	email varchar2(100) UNIQUE ,  		-- 이메일. 널값은 허용합니다.
-	join_date DATE default sysdate  -- default 는 기본값 키워드. sysdate 는 현재날짜와 시간  	
+	join_date DATE default sysdate  -- default 는 기본값 키워드. 오라클 sysdate 는 현재날짜와 시간  	
 				-- 가입날짜 , 날짜형식 : insert 할 때 날짜 패턴 'yyyy-mm-dd' 문자열이 자동으로 날짜형식 변환
 				-- 출력형식 :  yyyy-mm-dd hh24:mi:ss
 );
@@ -14,9 +14,14 @@ SELECT * FROM TBL_MEMBER  ;
 INSERT INTO 
 	TBL_MEMBER			 
 VALUES 
-	(1,'김모모','momo@naver.com','2022-11-24');  -- '2022-11-24' 기본 패턴은 날짜 타입 자동 변환
+	(1,'김모모','momo@naver.com',to_date('2022-11-24','yyyy-mm-dd'));  
+-- '2022-11-24' 기본 패턴은 날짜 타입 자동 변환
 
-INSERT INTO TBL_MEMBER VALUES (2,'박나연','parkny@gmail.com','2023-10-24');
+INSERT INTO TBL_MEMBER VALUES (2,'박나연','parkny@gmail.com',to_date('2023-10-24','yyyy-mm-dd'));
+
+-- ⭕현재 세션에 대해 날짜 포맷 문자열 설정하기(vs code sql developer 는 직접 설정해야합니다.)
+ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD';
+-- ⭕ 위의 설정값 후에는 to_date() 없이 문자열로 자동 변환
 INSERT INTO TBL_MEMBER VALUES (3,'최슬기','slgichoi@naver.com','2024-09-24');
 INSERT INTO TBL_MEMBER VALUES (4,'박사나','san@gmail.com','2023-10-05');
 INSERT INTO TBL_MEMBER VALUES (5,'이나연','nayeon@gmail.com','2024-10-05');
@@ -49,9 +54,9 @@ ORDER BY JOIN_DATE DESC ;
  WHERE EMAIL LIKE '%@gmail.com' ;
  
 
--- 날짜 기본 함수 : 1) 날짜 타입을 문자열로 변환 to_char()    2) 문자열을 날짜타입으로 변환 to_date() 
+-- 🔥날짜 기본 함수 : 1) 날짜 타입을 문자열로 변환 to_char()    2) 문자열을 날짜타입으로 변환 to_date() 
 -- 2) 번 예시
-INSERT INTO TBL_MEMBER VALUES (10,'홍길동','gdHong@naver.com','20250104');
+INSERT INTO TBL_MEMBER VALUES (10,'홍길동','gdHong@naver.com','20250104');  -- 오류? 
 SELECT * FROM TBL_MEMBER tm ;
 
 -- 시간 포함한 문자열은 to_date 함수 필요. 
@@ -66,6 +71,7 @@ ORDER BY "jtime";	  -- 정렬 기준은 함수실행 결과
 
 
 -- 2023년도에 가입한 회원만 조회(정렬은 먼저 가입한 날짜 순서대로) : to_char() 함수
+--  'yyyy' 는 to_char() 문자열 변환 패턴
   SELECT * FROM TBL_MEMBER
   WHERE TO_CHAR(JOIN_DATE,'yyyy') ='2023'
   ORDER BY JOIN_DATE ;
