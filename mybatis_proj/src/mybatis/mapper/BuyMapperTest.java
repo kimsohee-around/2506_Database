6c8f03498f6a5c7e6f4212f0eaee44b256e6f38b
kimsohee <koreait.sec2020@gmail.com>
2025-08-07 09:59:54 +0900

    mybatis 매퍼인터페이스복습

--- mybatis_proj/src/mybatis/mapper/BuyMapperTest.java
+++ mybatis_proj/src/mybatis/mapper/BuyMapperTest.java
@@ -4,10 +4,10 @@ public class BuyMapperTest {
   public static void main(String[] args) {
     BuyMapperDao dao = new BuyMapperDao();
     String pcode = "APLE5kg"; // DOWON123a, JINRMn5 , APLE5kg ,
-    System.out.println(pcode + " 상품의 구매 내역 : " + dao.selectByPcode(pcode));
+    System.out.println(pcode + " 상품의 고객 구매 내역 : " + dao.selectByPcode(pcode));
     System.out.println(pcode + " 상품의 구매 수량 합계 : " + dao.selectSumByPcode(pcode));
     System.out.println(" 2024 년도 상품 구매 내역 : " + dao.selectByYear("2024"));
-    String cutomerId = "mina012"; // mina012, twice, hongGD
-    System.out.println(cutomerId + " 상품의 구매 내역 : " + dao.selectByCustomer(cutomerId));
+    String cutomerId = "wonder"; // mina012, twice, hongGD , wonder
+    System.out.println(cutomerId + "고객의 상품 구매 내역 : " + dao.selectByCustomer(cutomerId));
   }
 }
